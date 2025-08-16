cd5a9deffe21568d6007f705b68b9c157348dcd9
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-08-16 11:17:58 +0300

    Revert "install next pwa"
    This reverts commit 9fd87dcf5feb9c0f51fbe753aed93cf0803a89bd.

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,7 @@
 import type { NextConfig } from "next";
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-  ],
-})
-
-const nextConfig: NextConfig = withPWA({
+const nextConfig: NextConfig = {
   /* config options here */
-});
+};
 
 export default nextConfig;
