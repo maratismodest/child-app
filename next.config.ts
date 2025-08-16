fed32fc9d3b442d717c601a5d156c5b7df8c41d8
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-08-16 11:25:08 +0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
+import type {NextConfig} from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const revision = crypto.randomUUID();
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+    cacheOnNavigation: true,
+    swSrc: "app/sw.ts",
+    swDest: "public/sw.js",
+    additionalPrecacheEntries: [{url: "/~offline", revision}],
+});
+
+const nextConfig: NextConfig = {}
+
+export default withSerwist(nextConfig);
