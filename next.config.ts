9fd87dcf5feb9c0f51fbe753aed93cf0803a89bd
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-08-16 11:13:27 +0300

    install next pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+  ],
+})
+
+const nextConfig: NextConfig = withPWA({
   /* config options here */
-};
+});
 
 export default nextConfig;
